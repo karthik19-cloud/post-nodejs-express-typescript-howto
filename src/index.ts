b11cda5a049815d67eb49c4c4a864b6fb918c496
javakarthik <32713545+javakarthik@users.noreply.github.com>
2021-01-12 16:43:28 +0530

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,4 @@ import { app } from './app';
 const port = 5000;
 
 // Start server
-app.listen(port, () => console.log(`Server is listening on port ${port}!`));
+app.listen(port, () => console.log(`Server is listening hola on port ${port}!`));
