ff7a429b8262133976c24d53f1792697a4682ffe
javakarthik <32713545+javakarthik@users.noreply.github.com>
2021-01-13 15:18:24 +0530

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,3 @@
 module.exports = {
-    clearMocks: true,
-    moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx'],
-    roots: ['<rootDir>/src'],
-    testEnvironment: 'node',
-    transform: {
-        '^.+\\.tsx?$': 'ts-jest',
-    },
+  verbose: true,
 };
